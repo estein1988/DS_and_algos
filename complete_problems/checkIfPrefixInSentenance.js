88dcae1643de0af08bd70bfe1fa63bf46282dffd
estein1988 <estein1988@gmail.com>
2021-01-25 08:55:14 -0700

    Add Check If a Word Occurs As a Prefix of Any Word in a Sentence solution.

---
+++ complete_problems/checkIfPrefixInSentenance.js
@@ -0,0 +1,21 @@
+var isPrefixOfWord = function(sentence, searchWord) {
+    let sentenceSplit = sentence.split(' ');
+    for(let word of sentenceSplit){
+        if(word.startsWith(searchWord)){
+            return (sentenceSplit.indexOf(word) + 1)
+        }
+    }
+    return -1
+};
+
+
+//alternate
+var isPrefixOfWord = function(sentence, searchWord) {
+    sentence = sentence.split(' ');
+    for (let i = 0; i < sentence.length; i++) {
+        if (sentence[i].substr(0, searchWord.length) == searchWord) {
+            return i + 1;
+        }
+    }
+    return -1;
+};
